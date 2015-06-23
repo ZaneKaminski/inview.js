af6de5493b7db6bf301bfe4c82a87d39c82d183b
Zane Kaminski <zanekaminski@gmail.com>
2015-06-23 01:56:25 -0400

    Agian?

--- inview.js
+++ inview.js
@@ -1,23 +1,17 @@
-{
-  "name": "InView",
-  "version": "1.0.0",
-  "authors": [
-    "Zane Kaminski <zanekaminski@gmail.com>"
-  ],
-  "description": "jQuery plugin for determining if elements are in view.",
-  "main": "inview.js",
-  "keywords": [
-    "jquery"
-  ],
-  "license": "MIT",
-  "dependencies": {
-    "jquery": "
-  }
-  "ignore": [
-    "**/.*",
-    "node_modules",
-    "bower_components",
-    "test",
-    "tests"
-  ]
-}
+/// <reference path="typings/jquery/jquery.d.ts"/>
+(function($) {
+    $.fn.inView = function(action) {
+        var $window = $(window);
+        var docViewTop = $window.scrollTop();
+        var thisTop = this.offset().top;
+        var thisBottom = thisTop + this.height();
+        
+        if (action == 'all') {
+            var docViewBottom = docViewTop + $window.height();
+            return ((thisBottom <= docViewBottom) && (thisTop >= docViewTop));
+        }
+        else { // action == 'any'
+            return docViewTop < thisBottom;
+        }
+    };
+ }(jQuery));
