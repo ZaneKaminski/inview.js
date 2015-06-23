b5a8875399dae1f58372767207e1ae421952d089
Zane Kaminski <zanekaminski@gmail.com>
2015-06-23 01:56:10 -0400

    Initial commit. Should work as-is.

---
+++ inview.js
@@ -0,0 +1,23 @@
+{
+  "name": "InView",
+  "version": "1.0.0",
+  "authors": [
+    "Zane Kaminski <zanekaminski@gmail.com>"
+  ],
+  "description": "jQuery plugin for determining if elements are in view.",
+  "main": "inview.js",
+  "keywords": [
+    "jquery"
+  ],
+  "license": "MIT",
+  "dependencies": {
+    "jquery": "
+  }
+  "ignore": [
+    "**/.*",
+    "node_modules",
+    "bower_components",
+    "test",
+    "tests"
+  ]
+}
